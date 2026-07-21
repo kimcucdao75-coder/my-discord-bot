218e383e45cae80384bf4b0c81548492a3ef5caf
kimcucdao75-coder <kimcucdao75@gmail.com>
2026-07-21 10:13:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"🎉 Bot {bot.user} đã online trên Render 24/24!")
+    print(f"🤖 Bot {bot.user} đã online trên Render 24/24!")
 
 @bot.command()
 async def hi(ctx):
@@ -18,6 +18,21 @@ async def hi(ctx):
 @bot.command()
 async def khen(ctx):
     await ctx.send("khuaaoxanh depzai lam do")
+
+@bot.command()
+async def gay(ctx):
+    await ctx.send("gay là bạn hả?")
+
+@bot.command()
+async def print(ctx, *, text: str):
+    if text.startswith('("') and text.endswith('")'):
+        content = text[2:-2]
+        await ctx.send(content)
+    elif text.startswith("('") and text.endswith("')"):
+        content = text[2:-2]
+        await ctx.send(content)
+    else:
+        await ctx.send('⚠️ Cú pháp sai rồi! Bạn phải gõ đúng dạng: `!print("nội dung")`')
 
 # Render sẽ tự đọc TOKEN từ biến môi trường
 token = os.environ.get('BOT_TOKEN')
